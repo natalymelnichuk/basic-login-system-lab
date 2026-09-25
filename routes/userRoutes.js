842e53acbacecdc917420257eb97d40f4e38c520
Natallia <ntshmlnchuk@gmail.com>
2026-09-25 15:23:35 -0400

    Lab completed

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,76 @@
 
 const router = require("express").Router();
 const User = require("../models/User.js");
+const jwt = require("jsonwebtoken");
 
-// Endpoint
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find().select("-password");
+    return res.status(200).json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: err.message });
+  }
+});
+
+
+router.post("/login", async (req, res) => {
+  try {
+    // 
+    const { email, password } = req.body;
+
+    //
+    const existingUser = await User.findOne({email})
+
+    if (!existingUser) {
+        return res.status(400).json({ message: "Incorrect email or password." });
+    }
+
+    //
+
+    const isCorrect = await existingUser.isCorrectPassword(password);
+
+    if (!isCorrect) {
+        return res.status(400).json({ message: "Incorrect email or password." });
+    }
+
+    // 
+    const payload = {
+        _id: existingUser._id,
+        username: existingUser.username
+    };
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: "1h"
+    });
+
+    // 
+    const userObject = existingUser.toObject();
+    delete userObject.password;
+
+    return res.status(200).json({
+        token,
+        user: userObject
+    })
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: err.message });
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 router.post("/register", async (req, res) => {
   try {
     // 1. Take username, email and password from req.body
